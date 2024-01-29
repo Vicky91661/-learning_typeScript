f00ecad4a9e13842205333603dd0317dcb454da6
Vicky91661 <kumar.vickysah72@gmail.com>
2024-01-29 12:58:06 +0530

    First Program

---
+++ firstApp/a.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeting(n) {
+    console.log("Hello ", n);
+}
+let n = "Vicky";
+greeting(n);
---
+++ firstApp/a.ts
@@ -0,0 +1,6 @@
+function greeting(n:string){
+    console.log("Hello ",n)
+}
+
+let n:string ="Vicky";
+greeting(n);
