0a63386237afa00fdb23846deac2a23dae7cb457
Vicky91661 <kumar.vickysah72@gmail.com>
2024-02-02 16:59:44 +0530

    learned interfaces

--- firstApp/a.js
+++ firstApp/a.js
@@ -1,6 +1,33 @@
 "use strict";
+// how to set a parameter in a function 
 function greeting(n) {
     console.log("Hello ", n);
 }
 let n = "Vicky";
 greeting(n);
+// how to make return type in a function
+function sum(num1, num2) {
+    return num1 + num2;
+}
+const ans = sum(10, 34);
+console.log(ans);
+// How to paas a function to a fucntion
+function delayedFun(fun) {
+    setTimeout(fun, 2000);
+}
+function callback() {
+    console.log("hello world");
+}
+delayedFun(callback);
+function printName(user) {
+    user.display("hey there");
+    console.log(user.name);
+}
+const user1 = {
+    name: "vicky",
+    phone: 9166124770,
+    display(s) {
+        console.log(s);
+    }
+};
+printName(user1);
--- firstApp/a.ts
+++ firstApp/a.ts
@@ -1,6 +1,45 @@
+// how to set a parameter in a function 
 function greeting(n:string){
     console.log("Hello ",n)
 }
 
 let n:string ="Vicky";
 greeting(n);
+
+// how to make return type in a function
+function sum(num1:number,num2:number):number{
+    return num1+num2;
+}
+const ans:number = sum(10,34);
+console.log(ans)
+
+
+// How to paas a function to a fucntion
+function delayedFun(fun:()=>void){
+    setTimeout(fun,2000)
+}
+function callback():void{
+    console.log("hello world");
+}
+delayedFun(callback)
+
+//Learningn the Interfaces
+interface User{
+    name:string;
+    phone?:number;
+    display(name:string):void;
+}
+
+function printName(user:User):void{
+    user.display("hey there")
+    console.log(user.name);
+}
+
+const user1={
+    name:"vicky",
+    phone:9166124770,
+    display(s:string):void{
+        console.log(s)
+    }
+}
+printName(user1)
